fed228a0c5a3a639c5ce2ac2b825e28955ecd190
devgaurav4912 <pawargaurav49122@gmail.com>
2024-08-29 19:03:02 +0530

    spring boot maven project is created

---
+++ Backend/BackInFormal-Backend/src/main/java/com/backinformal/BackInFormal_Backend/BackInFormalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.backinformal.BackInFormal_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackInFormalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackInFormalBackendApplication.class, args);
+	}
+
+}
