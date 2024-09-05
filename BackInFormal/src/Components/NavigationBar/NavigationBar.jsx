29241250c9f241f02d932e8e60d3e988674dc974
NikhilD-1007 <nikhildhage1@gmail.com>
2024-09-05 16:04:57 +0530

    Solve some bugs

--- BackInFormal/src/Components/NavigationBar/NavigationBar.jsx
+++ BackInFormal/src/Components/NavigationBar/NavigationBar.jsx
@@ -1,29 +1,20 @@
-import React, { useState } from 'react'
-import './navbar.css'
-import logo from './image.png'
+import React, { useState } from 'react';
+import './navbar.css';
+import logo from './image.png';
 import { Row, Col, Card, Button } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 import { useAuth } from '../Contexts/AuthContext';
 import { toast } from 'react-toastify';
-import createLogo from '../../assets/createInvoice.png'  
-import ListIcon from '../../assets/ListIcon.png'
-import settingLogo from '../../assets/settingLogo.png'
-import logoutIcon from '../../assets/logoutIcon.png'
+import createLogo from '../../assets/createInvoice.png';
+import ListIcon from '../../assets/ListIcon.png';
+import settingLogo from '../../assets/settingLogo.png';
+import logoutIcon from '../../assets/logoutIcon.png';
 
-
-
-
-
-function NavigationBar({navTitle,setNavTitle}) {
+function NavigationBar({ navTitle, setNavTitle }) {
   const { logout } = useAuth();
 
-
-
-
-
-
   return (
-    <div className='navbarDiv'>
+    <div className="navbarDiv">
       <Row>
         <Col xs={10}>
           <Row>
@@ -32,28 +23,58 @@ function NavigationBar({navTitle,setNavTitle}) {
           <Row>
             <Col xs={2}>
               <NavLink to="/dashboard/" className="nav-link">
-                <Button className='navCreateInvoiceBtn' onClick={() => setNavTitle('Create Invoice')}> 
-                  <img src={createLogo} alt="" style={{width:'20px',marginRight:'5px'}} /> Create Invoice</Button>
+                <Button
+                  className="navCreateInvoiceBtn"
+                  onClick={() => setNavTitle('Create Invoice')}
+                >
+                  <img
+                    src={createLogo}
+                    alt=""
+                    style={{ width: '20px', marginRight: '5px' }}
+                  />{' '}
+                  Create Invoice
+                </Button>
               </NavLink>
             </Col>
             <Col xs={2}>
               <NavLink to="/dashboard/listinvoice" className="nav-link">
-                <Button className='navListInvoiceBtn' onClick={() => setNavTitle('List Invoice')}>
-                <img src={ListIcon} alt="" style={{width:'20px',marginRight:'10px'}} />
-                 List Invoice</Button>
+                <Button
+                  className="navListInvoiceBtn"
+                  onClick={() => setNavTitle('List Invoice')}
+                >
+                  <img
+                    src={ListIcon}
+                    alt=""
+                    style={{ width: '20px', marginRight: '10px' }}
+                  />
+                  List Invoice
+                </Button>
               </NavLink>
             </Col>
             <Col xs={2}>
               <NavLink to="/dashboard/settings" className="nav-link">
-                <Button className='navBusinessSettingBtn' onClick={() => setNavTitle('Business Setting')}> 
-                <img src={settingLogo} alt="" style={{width:'20px',marginRight:'5px'}} />
-                Business Setting</Button>
+                <Button
+                  className="navBusinessSettingBtn"
+                  onClick={() => setNavTitle('Business Setting')}
+                >
+                  <img
+                    src={settingLogo}
+                    alt=""
+                    style={{ width: '20px', marginRight: '5px' }}
+                  />
+                  Business Setting
+                </Button>
               </NavLink>
-            </Col>  
+            </Col>
             <Col xs={2}>
-              <Button className='navLogoutBtn' onClick={logout}>
-              <img src={logoutIcon} alt="" style={{width:'18px',marginRight:'10px'}} />
-              Logout</Button>
+              <Button className="navLogoutBtn" onClick={logout}>
+                <img
+                  src={logoutIcon}
+                  alt=""
+                  style={{ width: '18px', marginRight: '10px' }}
+                />
+                Logout
+              </Button>
             </Col>
           </Row>
         </Col>
@@ -63,7 +84,7 @@ function NavigationBar({navTitle,setNavTitle}) {
       </Row>
       {/* <hr /> */}
     </div>
-  )
+  );
 }
 
-export default NavigationBar
+export default NavigationBar;
